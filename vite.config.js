3278f302f8de8b7d5137daff2a044a08d663c8a5
Mayank <mayankyadav0202@gmail.com>
2025-12-31 18:15:33 +0530

    ;;

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
-  base: '/testt.io/'
+  base: '/testt.io/',
   plugins: [react()]
 }))
